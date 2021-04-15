137f5dc9fb1951891d671fc3415c28e96466f88d
James Slanger <jim@soniqualia.com>
2021-04-14 22:17:06 -0700

    error correction

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,8 @@ export default function Home() {
 
         <p className="description">
           Or just try and get CI working...
-            currently at one line deployment.
+            currently at one line deployment. Error Correct
+            
           </p>
 
 
